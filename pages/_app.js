f2d3ebd2a67ddff3e940a1dc6e4289daf60a1d69
ange-spirituel <orianeoyono@gmail.com>
2025-06-20 20:45:14 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
-// pages/_app.js
 import '../styles/globals.css';
+import { useRouter } from 'next/router'
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  const noBackgroundPages = ['/about']
+
+  const shouldHideBackground = noBackgroundPages.includes(router.pathname)
+
   return (
-    <div className="bg-angel min-h-screen text-white">
+    <div className={shouldHideBackground ? 'no-background' : 'with-background'}>
       <Component {...pageProps} />
     </div>
-  );
+  )
 }
