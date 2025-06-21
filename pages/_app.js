6d29412a949351a89874479fb7bce8089f1041ab
ange-spirituel <orianeoyono@gmail.com>
2025-06-21 02:14:08 +0200

    Corriger le conflit et mettre à jour _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 import '../styles/globals.css';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
+  const router = useRouter();
 
-  const noBackgroundPages = ['/about']
-
-  const shouldHideBackground = noBackgroundPages.includes(router.pathname)
+  const noBackgroundPages = ['/about'];
+  const shouldHideBackground = noBackgroundPages.includes(router.pathname);
 
   return (
     <div className={shouldHideBackground ? 'no-background' : 'with-background'}>
       <Component {...pageProps} />
     </div>
-  )
+  );
+}
